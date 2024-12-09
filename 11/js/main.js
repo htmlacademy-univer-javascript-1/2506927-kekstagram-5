ee5a040b07dbad4ad7782fa5c21269d919cb91aa
Keks <keks@htmlacademy.ru>
2024-12-09 20:45:53 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,29 @@
+import './util.js';
+import {createPost} from './data.js';
+import {renderPictures} from './pictureRenderer.js';
+import {renderBigPicture} from './bigPictureViewer.js';
+import {formValidation} from './formValidation.js';
+
+const posts = [];
+for (let i = 0; i < 25; i++) {
+  posts.push(createPost());
+}
+
+renderPictures(posts);
+
+const picturesContainer = document.querySelector('.pictures');
+
+const handlePictureClick = function(evt) {
+  const pictureElement = evt.target.closest('.picture');
+  if (pictureElement) {
+    const postId = pictureElement.getAttribute('data-id');
+    const post = posts.find((p) => p.id === Number(postId));
+    if (post) {
+      renderBigPicture(post);
+    }
+  }
+};
+
+picturesContainer.addEventListener('click', handlePictureClick);
+
+formValidation();
