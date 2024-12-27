80faeae857ee061c7b6d8b001d515d020af39032
Keks <keks@htmlacademy.ru>
2024-12-27 20:12:33 +0300

    :heavy_check_mark: Сборка #15

---
+++ 15/js/main.js
@@ -0,0 +1,8 @@
+import './util.js';
+import './pictureRenderer.js';
+import './bigPictureViewer.js';
+import './api.js';
+import { validateForm } from './formValidation.js';
+import './filters.js';
+
+validateForm();
---
+++ 15/js/util.js
@@ -0,0 +1,16 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export {getRandomInteger, debounce};
